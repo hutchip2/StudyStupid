8ab258d5e3ea27c7c000fdac74e7b7cd3047ad8d
Paul Hutchinson <paulh@projectdragonfly.org>
2013-10-17 14:53:18 -0400

    cleaning up javascript

--- app/assets/javascripts/flashcards.js
+++ app/assets/javascripts/flashcards.js
@@ -2,13 +2,90 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-
-
-// Keydown function for flipping flashcard
 $(document).ready(function() {
+  makeFlashcardCanvas();
+  buildShowEditDestroy();
+  buildLeftScroll();
+  buildRightScroll();
+  flipFlashcard();
+});
+
+function makeFlashcardCanvas() {
+  window.flashcards = new Array();
+  window.current_card = 0;
+  var canvas = document.getElementById("card");
+  var context = canvas.getContext('2d');
+  var x = canvas.width / 3;
+  var y = canvas.height / 2;
+  context.font = '20pt Calibri';
+  context.fillStyle = 'blue';
+  context.fillText("Flashcards", x, y);
+}
+
+function buildShowEditDestroy() {
+  var deckCount = parseInt($('#deckCount').text(), 10);
+  for (var i = 0; i < deckCount; i++) {
+    var show     = document.getElementById("show"+i);
+    var edit     = document.getElementById("edit"+i);
+    var destroy  = document.getElementById("destroy"+i);
+    var showctx     =    show.getContext('2d');
+    var editctx     =    edit.getContext('2d');
+    var destroyctx  = destroy.getContext('2d');
+    showctx.font = '150pt Calibri';
+    showctx.fillStyle = 'black';
+    showctx.fillText('S', show.width/3, show.height);
+    editctx.font = '150pt Calibri';
+    editctx.fillStyle = 'black';
+    editctx.fillText('E', edit.width/3, edit.height);
+    destroyctx.font = '150pt Calibri';
+    destroyctx.fillStyle = 'black';
+    destroyctx.fillText('D', destroy.width/3, destroy.height);
+    show.onclick = function() {
+      window.location = "/decks/"+i;
+    }
+    edit.onclick = function() {
+      window.location = "/decks/"+ i +"/edit";
+    }
+    destroy.onclick = function() {
+      //window.location = "/decks/<%=deck.id%>";
+      //<td><%= link_to 'Destroy', deck, method: :delete, data: { confirm: 'Are you sure?' } %></td>
+    }
+  }
+}
+
+function buildLeftScroll() {
+  $('#button_left').each(function () {
+    var hovered = false;
+    var loop = window.setInterval(function () {
+      if (hovered) {
+        $("#button_left").css("color", "red");
+        var leftPos = $('#decks_outer').scrollLeft();
+        $('#decks_outer').animate({scrollLeft: leftPos - 50});
+      } else {
+        $("#button_left").css("color", "blue"); }
+    }, 500);
+    $(this).hover( function () { hovered = true; }, function () { hovered = false;} );
+  });
+}
+
+function buildRightScroll() {
+  $('#button_right').each(function () {
+    var hovered = false;
+    var loop = window.setInterval(function () {
+      if (hovered) {
+        $("#button_right").css("color", "red");
+        var leftPos = $('#decks_outer').scrollLeft();
+        $('#decks_outer').animate({scrollLeft: leftPos + 50});
+      } else {
+        $("#button_right").css("color", "blue"); }
+    }, 500);
+    $(this).hover( function () { hovered = true; }, function () { hovered = false;} );
+  });
+}
+
+function flipFlashcard() {
   $(document).keydown(function(e) {
     if (e.keyCode == '32') {
-      //alert(window.flashcards.length);
       window.current_card++;
       if (window.current_card >= window.flashcards.length)  {
         window.current_card = 0;
@@ -39,61 +116,4 @@ $(document).ready(function() {
       context.fillText(curCard, x, y);
     }
   });
-});
-
-// Make flashcard canvas
-$(document).ready(function() {
-
-  window.flashcards = new Array();
-  window.current_card = 0;
-
-  var canvas = document.getElementById("card");
-  var context = canvas.getContext('2d');
-  var x = canvas.width / 3;
-  var y = canvas.height / 2;
-  context.font = '20pt Calibri';
-  context.fillStyle = 'blue';
-  context.fillText("Flashcards", x, y);
-});
-
-// Left scroll function for deck button
-$(document).ready(function() {
-  $('#button_left').each(function () {
-    var hovered = false;
-    var loop = window.setInterval(function () {
-      if (hovered) {
-        $("#button_left").css("color", "red");
-        var leftPos = $('#decks_outer').scrollLeft();
-        $('#decks_outer').animate({scrollLeft: leftPos - 50});
-      } else {
-        $("#button_left").css("color", "blue"); }
-    }, 500);
-    $(this).hover( function () { hovered = true; }, function () { hovered = false;} );
-  });
-});
-
-/*$(document).ready(function() {
-  $("#button_left").mouseover(function() {
-    $("#button_left").css("color", "red");
-    var leftPos = $('#decks_outer').scrollLeft();
-    $('#decks_outer').animate({scrollLeft: leftPos - 10});
-  }).mouseout(function(){
-    $("#button_left").css("color", "blue");
-  });
-});*/
-
-// Right scroll function for deck button
-$(document).ready(function() {
-  $('#button_right').each(function () {
-    var hovered = false;
-    var loop = window.setInterval(function () {
-      if (hovered) {
-        $("#button_right").css("color", "red");
-        var leftPos = $('#decks_outer').scrollLeft();
-        $('#decks_outer').animate({scrollLeft: leftPos + 50});
-      } else {
-        $("#button_right").css("color", "blue"); }
-    }, 500);
-    $(this).hover( function () { hovered = true; }, function () { hovered = false;} );
-  });
-});
+}
